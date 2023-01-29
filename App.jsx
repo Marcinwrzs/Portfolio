8d05a576dac66f034d49c073b601c8ca72739bfa
Marcinwrzs <77743373+Marcinwrzs@users.noreply.github.com>
2023-01-29 16:44:33 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React, {useContext} from 'react';
+import {GlobalContext} from './context/Globalstate';
+import { HashRouter as Router} from 'react-router-dom';
+import Header from 'header/Header';
+import Pages from 'pages/Pages';
+import { Wrap } from './App.styled';
+
+const App = () => {
+  const {isOpen, darkMode} = useContext(GlobalContext);
+  
+  return (
+      <Router>
+        <Wrap darkMode={darkMode}>
+            <Header />
+            <Pages />
+        </Wrap>
+      </Router>
+  );
+}
+
+export default App;
